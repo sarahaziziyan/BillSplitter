1bad96946df9971a8adb62f03a88b243e673bb73
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-09-19 23:03:38 +0330

    write the Splitting calss

--- src/java/core/Splitting.java
+++ src/java/core/Splitting.java
@@ -7,6 +7,38 @@ import beans.Person;
 public class Splitting {
     
     public Owes split(Person[] people, Expense[] expenses){
+        Owes owes[] = new Owes[people.length-1];
+        
+        float sumExpenses = 0;
+        float owesToTreasury = 0;
+
+        for (Expense expense : expenses) {
+            sumExpenses += expense.getAmount();
+        }
+        
+        owesToTreasury = sumExpenses/people.length;
+        Person creditor = null;
+        for(Person person : people){
+            float howMuchOwesToTreasury = person.getHowMuchSpent() - owesToTreasury;
+            if(howMuchOwesToTreasury > 0 ){
+                creditor =  person;
+            }
+            person.setHowMuchOwesToTreasury(howMuchOwesToTreasury);
+            
+        }        
+        
+        for(int i = 0; i < people.length; i++){
+            Person debtor = null;
+            int owesCount = 0;
+            if(people[i].getHowMuchOwesToTreasury() < 0){
+                debtor = people[i];
+                owes[owesCount] = new Owes(debtor, creditor, Math.abs(people[i].getHowMuchOwesToTreasury()));
+                System.out.println(debtor.getName()+" owes " + Math.abs(people[i].getHowMuchOwesToTreasury()) + " to " + creditor.getName());
+                owesCount++;
+            }
+        }
+        
+        
         
         
         
