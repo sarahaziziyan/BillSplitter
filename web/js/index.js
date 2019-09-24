42bbec66f3b8ac621a96db76cdbd4444e97682ee
Sarah Aziziyan <aziziyan.sarah@yahoo.com>
2019-09-24 23:18:46 +0330

    made a json to send data to the server

--- web/js/index.js
+++ web/js/index.js
@@ -2,17 +2,15 @@
 $(document).ready(function(){
     
     $("#submit").click(function(){
-        var people = "";
-        $.each($(".person"),function(){
-            people += $(".person").val();  
-        })
-        
+        var people      = createJSONPeople();
+        var expenses    = createJSONExpenses();
         $.post("BillSplitterServlet",
             {
-                people : people       
+                people   : people,
+                expenses : expenses
             },
             function(mydata, status){
-
+                alert(status);
             }
         );
     });
@@ -20,7 +18,7 @@ $(document).ready(function(){
     $("#addPerson").click(function(){
         //var personHtml = '<div class="block"><label class="personLabel">Name:</label><input type="text" class="person" placeholder="name"/></div>';
         var personName = $("#personContainer").children("div.block:last").find("input").val();
-        $("#personContainer").children("div.block:first").before("<div clsss='addedPerson'>person: "+personName+"</div>");
+        $("#personContainer").children("div.block:first").before($("<div class='addedPerson' personName='"+personName+"'>person: "+personName+"</div>"));
         $("#peopleSelectBox").append("<option value="+personName+">"+personName+"</option>");
         $("#personContainer").children("div.block:last").find("input").val("");
     });
@@ -30,7 +28,7 @@ $(document).ready(function(){
         var expenseName     = lastChild.find(".expense").val();
         var amount          = lastChild.find(".amount").val();
         var personWhoSpent  = $("#peopleSelectBox").children("option:selected").val()
-        lastChild.before("<div clsss='addedExpense'>Expense: "+personWhoSpent+" spent "+amount+"$ on"+expenseName+"</div>");
+        lastChild.before("<div class='addedExpense' expenseName='"+expenseName+"' amount='"+amount+"' personWhoSpent='"+personWhoSpent+"'>Expense: "+personWhoSpent+" spent "+amount+"$ on"+expenseName+"</div>");
         
         lastChild.find(".expense").val("");
         lastChild.find(".amount").val("");
@@ -38,3 +36,31 @@ $(document).ready(function(){
     });
     
 });
+
+function createJSONPeople() {
+    jsonObj = [];
+    $("#personContainer").find(".addedPerson").each(function() {
+        var personName = $(this).attr("personName");
+        item = {}
+        item ["personName"] = personName;
+        jsonObj.push(item);
+    });
+    return jsonObj;
+    console.log(jsonObj);
+}
+
+function createJSONExpenses() {
+    jsonObj = [];
+    $("#expenseContainer").find(".addedExpense").each(function() {
+        var expensename = $(this).attr("expensename");
+        var amount = $(this).attr("amount");
+        var personwhospent = $(this).attr("personwhospent");
+        item = {}
+        item ["expensename"] = expensename;
+        item ["amount"] = amount;
+        item ["personWhoSpent"] = personwhospent;
+        jsonObj.push(item);
+    });
+    return jsonObj;
+    console.log(jsonObj);
+}
